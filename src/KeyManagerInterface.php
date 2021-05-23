d0ccc6f3bb059c334d9468b9dfeccaf783e7cc65
Abraham Udele <mac@Abrahams-Macbook-Pro.local>
2021-05-23 07:37:37 +0100

    Refactor

--- src/KeyManagerInterface.php
+++ src/KeyManagerInterface.php
@@ -5,7 +5,6 @@ use Illuminate\Http\JsonResponse;
 
 interface KeyManagerInterface
 {
-  
     public function createClient(string $name, string $type, string $status = 'active'): JsonResponse;
 
     public function getPrivateKey(int $client_id): JsonResponse;
